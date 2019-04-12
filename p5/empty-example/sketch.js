59a4408092c929d14b668359a0d33305609d73c0
SenpaiHawk <magnus.hgk@gmail.com>
2019-04-12 13:50:53 +0200

    Skit
    COMMITA SKITTA

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  // put setup code here
+  createCanvas(500,100);
+  fontRegular = loadFont('../../WORDSOFLOVE.ttf');
+  frameRate(2);
+
+}
+
+function draw() {
+  // put drawing code here
+  background(random(1,255),random(1,255),random(1,255));
+  textFont(fontRegular);
+  textSize(50);
+  text("seks måneder med dig",width/12,height/1.6);
+
+
+}
+// plz work
